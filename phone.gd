cb4a20b9df13149b090bfed9f6afeedb41b0a331
ghultquist <ghultquist@bsu.edu>
2022-06-17 21:55:39 -0400

    Create Main scene

--- phone.gd
+++ phone.gd
@@ -8,18 +8,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$OpeningAni.play("Opening")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func intro():
+	$OpeningAni.play("Opening")
 
 func _on_StartBtn_pressed():
-	intro()
+	gamestart()
 
-func intro():
+func gamestart():
 	$StartBtn.hide()
 	$EndingsBtn.hide()
 	$CreditsBtn.hide()
